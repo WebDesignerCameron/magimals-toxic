2e13e5f100b683f8c28070332390afb9c0547409
Cameron <webdesignercameron@gmail.com>
2026-09-09 17:44:26 +0100

    Fix syntax error in x array of trace1

--- plot.js
+++ plot.js
@@ -1,6 +1,6 @@
 const plotSection = document.getElementById('plotSection');
 const trace1 = {
-    x: ["Jan 2026", "Mar 2026" "May 2026", "Jul 2026", "Sep 2026"],
+    x: ["Jan 2026", "Mar 2026", "May 2026", "Jul 2026", "Sep 2026"],
     y: [0, 3, 4, 6, 9],
     type: 'scatter',
     mode: 'lines+markers',
